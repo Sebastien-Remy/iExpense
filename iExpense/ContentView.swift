2056589cab9a81308107428af6a06bcff0d699ca
Sebastien REMY <seb.remy@me.com>
2020-06-14 10:36:41 +0200

    Initial Commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Sebastien REMY on 14/06/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
